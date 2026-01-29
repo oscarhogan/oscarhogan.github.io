f904c52e6729dc32c1500ae4343b138da82e9e15
oscarhogan <oscar1hogan@gmail.com>
2026-01-29 04:49:05 +0000

    fixing images

--- proc/MCEVis.py
+++ proc/MCEVis.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import os
+import geopandas as gpd
 
-gdf = gpd.read_file("Documents/OscarWeb/data/areabounds.shp")
-popden = pd.read_csv("Documents/OscarWeb/data/PopDenNomis.csv")
 
+gdf = gpd.read_file("data/areabounds.shp")
+gdf = gdf.to_crs("EPSG:4326")
+popden = pd.read_csv("data/PopDenNomis.csv")
+lightloc = pd.read_csv("data/LightLoc.csv")
 
 merged = gdf.merge(popden, left_on="OA21CD", right_on="2021 output area")
 
-print(merged.columns)
+print(merged.crs)
 
-merged2 = 
-#merged.plot(column="Density", cmap="OrRd", legend=True)
-#plt.show()
+print(lightloc.head())
+
+print(lightloc.head)
