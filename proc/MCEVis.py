9a6010c87cb94928eb41dfa8251bf776b2487d45
oscarhogan <oscar1hogan@gmail.com>
2026-01-29 06:14:46 +0000

    Getting light files to compile for practice

--- proc/MCEVis.py
+++ proc/MCEVis.py
@@ -1,3 +1,4 @@
+from numpy import rint
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -6,14 +7,25 @@ import geopandas as gpd
 
 
 gdf = gpd.read_file("data/areabounds.shp")
-gdf = gdf.to_crs("EPSG:4326")
 popden = pd.read_csv("data/PopDenNomis.csv")
-lightloc = pd.read_csv("data/LightLoc.csv")
+Southwarklightloc = pd.read_csv("data/LightLoc.csv")
+Lewishamlightloc = gpd.read_file("data/Lewisham.shp")
+Lambethlightloc = gpd.read_file("data/Street_Lighting.shp")
 
+gdf = gdf.to_crs("EPSG:4326")
+
+popden = pd.read_csv("data/PopDenNomis.csv")
 merged = gdf.merge(popden, left_on="OA21CD", right_on="2021 output area")
 
-print(merged.crs)
+lightlocgdf = gpd.GeoDataFrame(Southwarklightloc, geometry=gpd.points_from_xy(Southwarklightloc.easting, Southwarklightloc.northing))
+Southwark = lightlocgdf.set_crs("EPSG:27700")
+Southwark = Southwark.to_crs("EPSG:4326")
 
-print(lightloc.head())
+Lewisham = Lewishamlightloc.set_crs("EPSG:4326")
+Lambeth = Lambethlightloc.to_crs("EPSG:4326")
 
-print(lightloc.head)
+print(Lewisham.geometry.head(5))
+print(Lambeth.geometry.head(5))
+print(Southwark.geometry.head(5))
+
+#Lewisham is fucked will fix tomoz
