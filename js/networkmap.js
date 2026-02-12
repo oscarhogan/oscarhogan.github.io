cb98967b36bdb760305cc9498d39eaec1b4e8d4d
oscarhogan <oscar1hogan@gmail.com>
2026-02-12 04:17:09 +0000

    PrivateScope

--- js/networkmap.js
+++ js/networkmap.js
@@ -1,30 +1,28 @@
-var map3 = L.map('networkmap').setView([51.462405, -0.066630], 12);
+(function() {
+    var map3 = L.map('networkmap').setView([51.462405, -0.066630], 12);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map3);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map3);
-setTimeout(function() {
-  map3.invalidateSize();
-}, 100);
+    setTimeout(function() { map3.invalidateSize(); }, 300);
 
-const styleNetwork = {
-  color: "#000000",  
-  weight: 2,         
-};
+    const styleNetwork = {
+        color: "#000000",  
+        weight: 2
+    };
 
-fetch('data/Network.geojson')
-  .then(res => res.json())
-  .then(data => {
-      L.geoJson(data, {style: styleNetwork}).addTo(map3);
-  });
+    fetch('data/Network.geojson')
+      .then(res => res.json())
+      .then(data => {
+          L.geoJson(data, {style: styleNetwork}).addTo(map3);
+      });
 
-// Title control for Map 1
-const Networktitle = L.control({ position: 'topright' });
+    const Networktitle = L.control({ position: 'topright' });
+    Networktitle.onAdd = function () {
+        const div = L.DomUtil.create('div', 'maptitle');
+        div.innerHTML = '<h4> South London Road/Pathway Network</h4>';
+        return div;
+    };
+    Networktitle.addTo(map3);
 
-Networktitle.onAdd = function () {
-    const div = L.DomUtil.create('div', 'maptitle');
-    div.innerHTML = '<h4> South London Road/Pathway Network</h4>';
-    return div;
-};
-
-Networktitle.addTo(map3);
+})();
--- js/swipemap.js
+++ js/swipemap.js
@@ -1,100 +1,82 @@
-// Map 1 - Density
+(function() {
+    // Map 1 - Density
+    var map = L.map('densitymap').setView([51.462405, -0.066630], 12);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
 
-var map = L.map('densitymap').setView([51.462405, -0.066630], 12);
+    setTimeout(function() { map.invalidateSize(); }, 100);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+    function getColorDensity(d) {
+        return d > 90652  ? '#BD0026' :
+               d > 46250  ? '#E31A1C' :
+               d > 25294  ? '#FC4E2A' :
+               d > 13085  ? '#FD8D3C' :
+                            '#FFEDA0'; 
+    }
 
-setTimeout(function() {
-  map.invalidateSize();
-}, 100);
+    function styleDensity(feature) {
+        return {
+            fillColor: getColorDensity(feature.properties.Density),
+            weight: 0.5,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 0.7
+        };
+    }
 
-// Color function for choropleth
-function getColorDensity(d) {
-    return d > 90652  ? '#BD0026' :
-           d > 46250  ? '#E31A1C' :
-           d > 25294  ? '#FC4E2A' :
-           d > 13085  ? '#FD8D3C' :
-                        '#FFEDA0'; 
-}
+    fetch('data/AreaWithLightDensity.geojson')
+      .then(res => res.json())
+      .then(data => {
+          L.geoJson(data, {style: styleDensity}).addTo(map);
+      });
 
-// Style function for each feature
-function styleDensity(feature) {
-    return {
-        fillColor: getColorDensity(feature.properties.Density), // Use your GeoJSON property
-        weight: 0.5,
-        opacity: 1,
-        color: 'white',
-        fillOpacity: 0.7
+    const Denstitle = L.control({ position: 'topright' });
+    Denstitle.onAdd = function () {
+        const div = L.DomUtil.create('div', 'maptitle');
+        div.innerHTML = '<h4>Population Density</h4>';
+        return div;
     };
-}
+    Denstitle.addTo(map);
 
-// Fetch GeoJSON and add to map with style
-fetch('data/AreaWithLightDensity.geojson')
-  .then(res => res.json())
-  .then(data => {
-      L.geoJson(data, {style: styleDensity}).addTo(map);
-  });
+    // Map 2 - Light Density
+    var map2 = L.map('lightdensitymap').setView([51.462405, -0.066630], 12);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map2);
 
-// Title control for Map 1
-const Denstitle = L.control({ position: 'topright' });
+    setTimeout(function() { map2.invalidateSize(); }, 100);
 
-Denstitle.onAdd = function () {
-    const div = L.DomUtil.create('div', 'maptitle');
-    div.innerHTML = '<h4>Population Density</h4>';
-    return div;
-};
+    function getColorLightDensity(d) {
+        return d > 3699  ? '#BD0026' :
+               d > 1630  ? '#E31A1C' :
+               d > 961  ? '#FC4E2A' :
+               d > 495  ? '#FD8D3C' :
+                            '#FFEDA0'; 
+    }
 
-Denstitle.addTo(map);
+    function styleLightDensity(feature) {
+        return {
+            fillColor: getColorLightDensity(feature.properties.lightdensi),
+            weight: 0.5,
+            opacity: 1,
+            color: 'white',
+            fillOpacity: 0.7
+        };
+    }
 
+    fetch('data/AreaWithLightDensity.geojson')
+      .then(res => res.json())
+      .then(data => {
+          L.geoJson(data, {style: styleLightDensity}).addTo(map2);
+      });
 
-// Map 2 - Light Density
-
-var map2 = L.map('lightdensitymap').setView([51.462405, -0.066630], 12);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map2);
-
-setTimeout(function() {
-  map2.invalidateSize();
-}, 100);
-
-// Color function for choropleth
-function getColorLightDensity(d) {
-    return d > 3699  ? '#BD0026' :
-           d > 1630  ? '#E31A1C' :
-           d > 961  ? '#FC4E2A' :
-           d > 495  ? '#FD8D3C' :
-                        '#FFEDA0'; 
-}
-
-function styleLightDensity(feature) {
-    return {
-        fillColor: getColorLightDensity(feature.properties.lightdensi), // Use your GeoJSON property
-        weight: 0.5,
-        opacity: 1,
-        color: 'white',
-        fillOpacity: 0.7
+    const Lighttitle = L.control({ position: 'topright' });
+    Lighttitle.onAdd = function () {
+        const div = L.DomUtil.create('div', 'maptitle');
+        div.innerHTML = '<h4>Street Light Density</h4>';
+        return div;
     };
-}
+    Lighttitle.addTo(map2);
 
-// Fetch GeoJSON and add to map with style
-fetch('data/AreaWithLightDensity.geojson')
-  .then(res => res.json())
-  .then(data => {
-      L.geoJson(data, {style: styleLightDensity}).addTo(map2);
-  });
-
-// Title control for Map 2
-
-const Lighttitle = L.control({ position: 'topright' });
-
-Lighttitle.onAdd = function () {
-    const div = L.DomUtil.create('div', 'maptitle');
-    div.innerHTML = '<h4>Street Light Density</h4>';
-    return div;
-};
-
-Lighttitle.addTo(map2);
+})();
